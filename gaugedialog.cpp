45f9dffd002e129078444703248cd16b0a794687
ibrahim <adnan@comspots.com>
2018-09-11 13:55:18 +0300

    -working with transparent background

--- gaugedialog.cpp
+++ gaugedialog.cpp
@@ -6,6 +6,7 @@
 #include <QPainter>
 #include <QTransform>
 
+#define NEEDLE_PATH ":/img/img/drag_pointer.png"
 
 static int rotation_value=0;
 //static QPixmap * p_needle;
@@ -14,14 +15,8 @@ GaugeDialog::GaugeDialog(QWidget *parent) :
     ui(new Ui::GaugeDialog)
 {
     ui->setupUi(this);
+    ui->needle_label->setPixmap(QPixmap(NEEDLE_PATH));
 
-    QPixmap needlepix(":/img/img/gauge-needle.png");
-
-    int w=ui->needle_label->width();
-    int h=ui->needle_label->height();
-    ui->needle_label->setPixmap(needlepix.scaled(w,h,Qt::KeepAspectRatio));
-    ui->needle_label->setAttribute(Qt::WA_TranslucentBackground);
-    ui->needle_label->repaint();
 
 }
 
@@ -33,46 +28,26 @@ GaugeDialog::~GaugeDialog()
 
 void GaugeDialog::on_forward_button_clicked()
 {
-//QPixmap right_rotate(*ui->needle_label->pixmap());
-//QMatrix rm;
-//rm.rotate(rotation_value);
-//   right_rotate=right_rotate.transformed(rm);
-//   ui->needle_label->setPixmap(right_rotate);
-//   ui->needle_label->setPixmap(needlepix);
 
-  QPixmap needle_pix(":/img/img/gauge-needle.png");
-  int w=ui->needle_label->width();
-      int h=ui->needle_label->height();
-  //    ui->needle_label->setPixmap(needle_pix.scaled(w,h,Qt::KeepAspectRatio));
-//  int w=ui->needle_label->width();
-//  int h=ui->needle_label->height();
-//  ui->needle_label->setPixmap(needle_pix.scaled(w,h,Qt::KeepAspectRatio));
-//  ui->needle_label->setAttribute(Qt::WA_TranslucentBackground);
-//   ui->needle_label->repaint();
-  QPixmap rotate(needle_pix.scaled(w,h,Qt::KeepAspectRatio));
+  QPixmap needle_pix(NEEDLE_PATH);
+  QPixmap rotate(needle_pix.size());
+  rotate.fill(Qt::transparent);
 
+  QPainter p(&rotate);
 
-//  QPainter p(&rotate);
+p.setRenderHint(QPainter::Antialiasing);
+p.setRenderHint(QPainter::HighQualityAntialiasing);
+p.setRenderHint(QPainter::SmoothPixmapTransform);
 
+p.translate(rotate.size().width()/2,rotate.size().height()/2);
+p.rotate(rotation_value+=45);
+p.translate(-rotate.size().width()/2,-rotate.size().height()/2);
 
-  QTransform transform;
+p.drawPixmap(0,0,needle_pix);
 
-  transform.translate(rotate.size().width()/2,rotate.size().height()/2);
-//  p.setRenderHint(QPainter::Antialiasing);
-//  p.setRenderHint(QPainter::SmoothPixmapTransform);
-//  p.setRenderHint(QPainter::HighQualityAntialiasing);
-//  p.translate(rotate.size().width()/2,rotate.size().height()/2);
-//  p.rotate(rotation_value);
-  transform.rotate(rotation_value+=10);
-//  p.translate(-rotate.size().width()/2,-rotate.size().height()/2);
- transform.translate(-rotate.size().width()/2,-rotate.size().height()/2);
-
-// p.drawPixmap(0,0,needle_pix);
-//p.end();
-
-ui->needle_label->setPixmap(rotate.transformed(transform));
-
-//ui->needle_label->repaint();
+p.end();
+ui->needle_label->setPixmap(rotate);
+ui->needle_label->repaint();
 
 
 }
